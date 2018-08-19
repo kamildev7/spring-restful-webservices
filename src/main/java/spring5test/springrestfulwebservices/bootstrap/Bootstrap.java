f430e7f35e2a1c81c77c9a52828140d237ffa20f
kamildev7 <k.93kamil@gmail.com>
2018-08-19 20:49:54 +0200

    Added Bootstrap data loaded. Created circleci config file.

---
+++ src/main/java/spring5test/springrestfulwebservices/bootstrap/Bootstrap.java
@@ -0,0 +1,47 @@
+package spring5test.springrestfulwebservices.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import spring5test.springrestfulwebservices.domain.Category;
+import spring5test.springrestfulwebservices.repositories.CategoryRepository;
+
+/**
+ * @author kamildev7 on 2018-08-19.
+ */
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+
+        System.out.println("Data Loaded = " + categoryRepository.count() );
+
+    }
+}
